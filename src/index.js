75c8cdc6f95f19cccdc7e2b02e02cf8cb416574a
manan <manang@webmynesystems.com>
2022-03-11 11:49:55 +0530

    fonts import error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./fonts/BasierCircle-Regular.otf";
+import "./assets/fonts/BasierCircle-Regular.otf";
 
 ReactDOM.render(
   <React.StrictMode>
